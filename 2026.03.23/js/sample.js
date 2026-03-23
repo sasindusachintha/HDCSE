76c2a2a15ed218225921adadabd84e8a5e4d8221
sasindusachintha <sasindusachintha1234@gmail.com>
2026-03-23 14:36:38 +0530

    aadded

---
+++ 2026.03.23/js/sample.js
@@ -0,0 +1,18 @@
+let day = 2 ;
+
+switch(day){
+    case 1 :
+        console.log("Monday");
+        break;
+    case 2 :
+        console.log("Tuesday");
+        break;
+    case 3 :
+        console.log("Wednesday");
+        break;
+    case 4 :
+        console.log("Friday");
+        break;
+    default : 
+    console.log("Invalid Day");
+}
