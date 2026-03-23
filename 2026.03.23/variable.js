0c45438d47749a18384da7fd8d249342167cce19
sasindusachintha <sasindusachintha1234@gmail.com>
2026-03-23 14:26:43 +0530

    aadded

--- 2026.03.23/variable.js
+++ 2026.03.23/variable.js
@@ -1,9 +1,34 @@
-let x = 10;
-let y= 20;
+// let x = 10;
+// let y= 20;
 
-console.log(x+y);
+// console.log(x+y);
 
 
-name = "sasindu" 
+// name = "sasindu" 
 
-console.log(name );
+// console.log(name );
+
+// let name = 'sasindu' ;
+// let age = 45 ;
+// let firstName = undefined ;
+// let selectColur = null ;
+
+// console.log("Check Type", typeof(name)) ;
+// console.log("Check Type", typeof(age)) ;
+// console.log("Check Type", typeof(firstName)) ;
+// console.log("Check Type", typeof(selectColur)) ;
+
+let score = 57;
+let grade;
+
+if(score >= 90){
+    grade = "A";
+}else if(score >= 80){
+    grade ="B"
+}else if(score >= 70){
+    grade = "c"
+}else{
+    grade = "D or F" ;
+}
+
+console.log("your grade is " + grade) ;
