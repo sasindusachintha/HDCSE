29f7c23897c84e7b5778801bc67e47b81157bad5
sasindusachintha <sasindusachintha1234@gmail.com>
2026-04-08 15:26:10 +0530

    added

---
+++ 2026_04_08/employee/insert.php
@@ -0,0 +1,22 @@
+<?php
+
+include 'db.php';
+
+if(isset($_POST['submit'])){
+
+   $name = $_POST['name'];
+   $email = $_POST['email'];
+   $position= $_POST['position'];
+   $salary = $_POST['salary'];
+
+
+   //prepare statement
+   $stmt = $conn->prepare("INSERT INTO employees (name, email, position, salary) VALUES (?,?,?,?) ");
+   $stmt->bind_param("sssd", $name, $email, $position, $salary);
+
+   if($stmt->execute()){
+    header("Location: index.php?success=1");
+   }else{
+    echo"Error: ". $conn->error;
+}
+}
