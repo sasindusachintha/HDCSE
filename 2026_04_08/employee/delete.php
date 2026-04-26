33d0005d7fd1b2849eb71ebb158dce242f31a7ee
sasindu <sasindusachintha1234@gmail.com>
2026-04-26 12:11:01 +0530

    enhanced code by commenting...

--- 2026_04_08/employee/delete.php
+++ 2026_04_08/employee/delete.php
@@ -1,9 +1,12 @@
 <?php
 include 'db.php';
 
+// Get the employee ID from the URL parameter
 $id = $_GET['id'];
 
+// Execute the delete query to remove the employee from the database
 $conn->query("DELETE FROM employees WHERE id=$id");
 
+// Redirect back to the main page after deletion
 header("Location: index.php");
 ?>
