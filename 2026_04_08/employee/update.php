2dea52d5541b0e944599e60e8eb6982417c96611
sasindu <sasindusachintha1234@gmail.com>
2026-04-26 12:09:05 +0530

    enhanced code

--- 2026_04_08/employee/update.php
+++ 2026_04_08/employee/update.php
@@ -1,17 +1,23 @@
 <?php
 include 'db.php';
 
+// Check if the update form is submitted
 if (isset($_POST['update'])) {
 
+// Get data from the form
     $id = $_POST['id'];
     $name = $_POST['name'];
     $email = $_POST['email'];
     $position = $_POST['position'];
     $salary = $_POST['salary'];
 
+    // Prepare and execute the update statement
     $stmt = $conn->prepare("UPDATE employees SET name=?, email=?, position=?, salary=? WHERE id=?");
+    
+    // Bind parameters (s for string, d for double, i for integer)
     $stmt->bind_param("sssdi", $name, $email, $position, $salary, $id);
 
+    // Execute the statement and check for success
     if ($stmt->execute()) {
         header("Location: index.php?updated=1");
     } else {
